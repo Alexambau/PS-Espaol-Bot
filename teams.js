4039a5236f19dd15c500ca7b7cdeafa809d6d650
Ecuacion <alajandro5gt01@gmail.com>
2015-01-30 18:43:50 +0100

    Add Multi-tier Teams

--- teams.js
+++ teams.js
@@ -1,9 +1,31 @@
 /* Bot Teams */
 
 exports.teams = {
-	"ou": [		'Bisharp||leftovers||ironhead,knockoff,suckerpunch,swordsdance|Adamant|232,252,,,4,20|||||]Chansey||eviolite||healbell,seismictoss,softboiled,thunderwave|Bold|248,,252,,8,|||||]Venusaur-Mega||venusaurite||gigadrain,hiddenpowerfire,leechseed,synthesis|Bold|240,,224,44,,||,30,,30,,30|||]Heatran||airballoon||stealthrock,fireblast,toxic,taunt|undefined|224,,,16,48,220|||||]Skarmory||rockyhelmet|1|roost,defog,spikes,bravebird|Impish|248,,232,,,28|||||]Keldeo||leftovers||scald,secretsword,substitute,calmmind|Timid|,,,252,4,252|||||',
+	"ou": [		
+		   'Bisharp||leftovers||ironhead,knockoff,suckerpunch,swordsdance|Adamant|232,252,,,4,20|||||]Chansey||eviolite||healbell,seismictoss,softboiled,thunderwave|Bold|248,,252,,8,|||||]Venusaur-Mega||venusaurite||gigadrain,hiddenpowerfire,leechseed,synthesis|Bold|240,,224,44,,||,30,,30,,30|||]Heatran||airballoon||stealthrock,fireblast,toxic,taunt|undefined|224,,,16,48,220|||||]Skarmory||rockyhelmet|1|roost,defog,spikes,bravebird|Impish|248,,232,,,28|||||]Keldeo||leftovers||scald,secretsword,substitute,calmmind|Timid|,,,252,4,252|||||',
 	'Lopunny||lopunnite|H|return,highjumpkick,icepunch,fakeout|Jolly|,252,4,,,252|||||]Gengar||lifeorb||shadowball,sludgewave,focusblast,taunt|Timid|4,,,252,,252|||||]Garchomp||focussash|H|stealthrock,earthquake,outrage,fireblast|Naive|,252,,4,,252|||||]Starmie||leftovers|1|scald,reflecttype,recover,rapidspin|Timid|232,,24,4,,248||,0,,,,|||]Bisharp||blackglasses||knockoff,ironhead,suckerpunch,swordsdance|Adamant|4,252,,,,252|||||]Thundurus||leftovers||focusblast,thunderbolt,hiddenpowerice,thunderwave|Timid|,,4,252,,252||,30,30,,,|||', 
 	'Charizard||charizarditex||dragondance,flareblitz,roost,dragonclaw|Jolly|96,220,,,8,184|M||||]Breloom||focussash|H|spore,rocktomb,bulletseed,machpunch|Jolly|4,252,,,,252|||||]Rotom-Wash||leftovers||voltswitch,hydropump,willowisp,painsplit|Bold|252,,212,,,44|||||]Landorus-Therian||choicescarf||knockoff,earthquake,stoneedge,uturn|Jolly|,252,4,,,252|||||]Jirachi||leftovers||ironhead,wish,stealthrock,thunderwave|Jolly|252,,,,4,252|||||]Latios||lifeorb||dracometeor,earthquake,hiddenpowerfire,defog|Naive|,,16,240,,252||,30,,30,,30|||',
 	'Sylveon||choicespecs|H|hypervoice,hiddenpowerfire,psyshock,batonpass|Modest|236,,,252,,20||,30,,30,,30|||]Manaphy||leftovers||scald,psychic,tailglow,raindance|Timid|96,,,252,,160|||||]Cobalion||leftovers||ironhead,closecombat,voltswitch,stealthrock|Jolly|140,116,,,,252|||||]Landorus-Therian||softsand||earthquake,stoneedge,knockoff,uturn|Adamant|,252,4,,,252|||||]Mega-Manectric|manectric|manectite|1|thunderbolt,voltswitch,flamethrower,hiddenpowerice|Timid|,,4,252,,252||,30,30,,,|||]Hydreigon||choicescarf||dracometeor,darkpulse,earthpower,uturn|Modest|,4,,252,,252|||||'
+	],
+	
+	"ubers": [
+			  'Latios||souldew||dracometeor,psyshock,recover,defog|Timid|,,,252,4,252||,0,,,,|||]Groudon-Primal||redorb||precipiceblades,firepunch,stoneedge,stealthrock|Adamant|252,252,,,,4|||||]Xerneas||powerherb||geomancy,moonblast,thunder,hiddenpowerfire|Modest|184,,,252,4,68||,30,,30,,30|||]Mewtwo-Mega-Y||mewtwonitey||psystrike,aurasphere,flamethrower,icebeam|Timid|4,,,252,,252|||||]Yveltal||leftovers||toxic,roost,foulplay,suckerpunch|Bold|224,,252,4,28,|||||]Scizor||choiceband|1|bulletpunch,uturn,superpower,knockoff|Adamant|,252,,,4,252|||||'
+	],
+	
+	"uu": [
+		   'Noctur|zygarde|leftovers||dragontail,earthquake,coil,substitute|Jolly|188,,,,104,216|||||]Axel|roserade|blacksludge||spikes,hiddenpowerfire,gigadrain,synthesis|Calm|248,,,8,252,||,30,,30,,30|||]Hyoga|swampert|leftovers||scald,stealthrock,earthquake,roar|Relaxed|252,4,252,,,|||||]Alastor Law|florges|leftovers||moonblast,wish,protect,aromatherapy|Bold|248,,252,,8,|||||]Madara prodigioso|hydreigon|lifeorb||dracometeor,darkpulse,fireblast,superpower|Hasty|,4,,252,,252|||||]beri isi flinch|jirachi|choicescarf||ironhead,uturn,zenheadbutt,healingwish|Jolly|,252,,,4,252|||||'
+	],
+	
+	"ru": [
+		   'panguillas|pangoro|lumberry|H|swordsdance,knockoff,drainpunch,icepunch|Adamant|,252,,,4,252|||||]tenasitas jr|gligar|eviolite||defog,roost,earthquake,uturn|Impish|248,8,252,,,|||||]slowpope|slowking|assaultvest|H|scald,fireblast,icebeam,psyshock|Modest|248,,,252,8,|||||]heavy metal|bronzong|leftovers|1|stealthrock,toxic,gyroball,rockslide|Sassy|248,8,,,252,||,,,,,0|||]frenzone|virizion|lumberry||swordsdance,closecombat,stoneedge,leafblade|Jolly|,252,,,4,252|||||]espiritu santo|pidgeotmega|pidgeotite||hurricane,heatwave,uturn,roost|Hasty|,4,,252,,252|||||'
+	],
+	
+	"nu": [
+		   'Huntail||whiteherb|H|shellsmash,waterfall,suckerpunch,hiddenpowergrass|Naughty|,252,,4,,252||,30,,30,,|||]Klinklang||leftovers|H|substitute,shiftgear,geargrind,frustration|Adamant|80,252,,,,176|||||0]Garbodor||rockyhelmet|H|gunkshot,drainpunch,spikes,toxicspikes|Impish|252,,196,,,60|||||]Piloswine||eviolite|H|iceshard,iciclecrash,earthquake,stealthrock|Adamant|240,252,,,,16|||||]Mismagius||colburberry||taunt,nastyplot,shadowball,thunderbolt|Timid|,,4,252,,252|||||]Kecleon||assaultvest|H|shadowsneak,suckerpunch,knockoff,drainpunch|Adamant|252,252,,,4,|||||'
+	],
+	
+	"lc": [
+		   'Zebra Bird|vullaby|eviolite|1|roost,defog,uturn,knockoff|Impish|,,236,,236,36||||5|]Smogonweasel|mienfoo|eviolite|1|drainpunch,uturn,knockoff,acrobatics|Jolly|,,36,,196,236||||5|]Chin Chang Chou|chinchou|eviolite||scald,voltswitch,sleeptalk,rest|Bold|76,,212,68,148,||,0,,,,||5|]Assaultvesta|larvesta|eviolite||morningsun,flareblitz,uturn,willowisp|Adamant|76,76,156,,156,36||||5|]Corporal|foongus|eviolite|H|spore,sludgebomb,gigadrain,hiddenpowerfighting|Bold|124,,160,,160,||,1,30,30,30,30||5|]Rainbows|archen|berryjuice||stealthrock,acrobatics,stoneedge,uturn|Jolly|76,180,,,,196||||5|',
+		   'Ole|chinchou|berryjuice||scald,voltswitch,healbell,thunderwave|Bold|76,,212,152,,60||||5|]Ole|mienfoo|choicescarf|1|highjumpkick,uturn,knockoff,drainpunch|Jolly|,236,36,,,236||||5|]Ole|ponyta|eviolite|H|flareblitz,morningsun,toxic,willowisp|Jolly|,76,236,,,196||||5|]Ole|vullaby|eviolite|1|bravebird,knockoff,roost,defog|Impish|116,76,236,,76,||||5|]Ole|diglett|focussash|1|earthquake,rockslide,suckerpunch,stealthrock|Jolly|,236,,,36,236||||5|]Y Ole|abra|lifeorb|H|psychic,hiddenpowerfighting,substitute,dazzlinggleam|Timid|,,,240,68,200||,,30,30,30,30||5|'
 	]
 };
